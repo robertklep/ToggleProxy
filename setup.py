8a9bbe126b342501a76e1900b0b517cfdab43ec5
Robert Klep <robert@klep.name>
2013-02-17 07:44:59 +0100

    Reverted icon files and updated setup.py to find them

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import  py2app, sys, os, commands
 
 setup(
     app         = [ 'ToggleProxy.py' ],
-    data_files  = glob('resources/icon-*.png'),
+    data_files  = glob('resources/*.png'),
     options     = dict(py2app = dict(
         plist   = dict(
             LSBackgroundOnly = True
