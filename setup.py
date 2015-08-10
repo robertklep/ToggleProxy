ed0de9fe18b17555ec24ba87796e0b827db3a401
Robert Klep <robert@klep.name>
2015-08-10 16:44:18 +0200

    Read version from Git tag

--- setup.py
+++ setup.py
@@ -2,8 +2,17 @@ from    distutils.core  import setup
 from    glob            import glob
 import  py2app, sys, os, commands
 
+# determine version by latest tag
+status, version = commands.getstatusoutput("git describe --abbrev=0 --tags")
+if status != 0:
+    # probably no hg installed or not building from a repository
+    version = "unknown"
+if version[0] == 'v':
+    version = version[1:]
+
 setup(
     app         = [ 'ToggleProxy.py' ],
+    version     = version,
     data_files  = glob('resources/*.png'),
     options     = dict(py2app = dict(
         plist   = dict(
